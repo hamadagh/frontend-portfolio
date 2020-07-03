43ac78fde2bdeac044bf538b765d147ea4695188
hamada <hamadagh777@gmail.com>
2020-07-03 22:38:47 +0200

    updating navbar style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,18 +4,18 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <div className="nav-bar">
-      <ul>
-        <li className="nav-link">
-          <Link to="/about" className="nav-link">
+      <ul className="nav">
+        <li className="list-item">
+          <Link to="/about" className="nav-link nav-link-about">
             ABOUT
           </Link>
         </li>
-        <li className="nav-link">
+        <li className="list-item">
           <Link to="/projects" className="nav-link">
             PROJECTS
           </Link>
         </li>
-        <li className="nav-link">
+        <li className="list-item">
           <Link to="/contact" className="nav-link">
             CONTACT
           </Link>
