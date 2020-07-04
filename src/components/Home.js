f35ca777af25eda74575f712bf6e48834d69b29f
hamada <hamadagh777@gmail.com>
2020-07-04 22:34:35 +0200

    adding icons to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,9 @@
 import React from "react";
 import Circle from "./images/circle.png";
+import Linkedin from "./images/linkedin.png";
+import Github from "./images/github.png";
+import Facebook from "./images/facebook.png";
+import Instagram from "./images/instagram.png";
 import "./style.css";
 
 function Home() {
@@ -8,10 +12,11 @@ function Home() {
       <div className="first-section">
         <span>Logo</span>
         <div>
-          <h3>Full-stack developer</h3>
-          <h5>
-            From <span>Berlin</span>
-          </h5>
+          <span className="full-stack">Full-stack developer</span>
+          <br></br>
+          <span className="from">
+            From <span className="berlin">Berlin</span>
+          </span>
         </div>
       </div>
       <div className="second-section">
@@ -20,12 +25,15 @@ function Home() {
         </div>
       </div>
       <div className="third-section">
-        <span>Logo</span>
-        <div>
-          <h3>Full-stack developer</h3>
-          <h5>
-            From <span>Berlin</span>
-          </h5>
+        <div className="my-name">
+          <span className="hamada">HAMADA</span>
+          <span className="ghanem">GHANEM</span>
+        </div>
+        <div className="icons">
+          <img src={Linkedin} alt="linkedin" className="icon" />
+          <img src={Github} alt="github" className="icon" />
+          <img src={Facebook} alt="facebook" className="icon" />
+          <img src={Instagram} alt="instagram" className="icon" />
         </div>
       </div>
     </div>
