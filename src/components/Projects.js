a4ef83a84ed66f38fb157448f3fb8791ca413825
hamada <hamadagh777@gmail.com>
2020-07-04 23:09:36 +0200

    adding projects in projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,7 +4,14 @@ import "./style.css";
 function Projects() {
   return (
     <div className="projects-page">
-      <h1>hey</h1>
+      <div className="projects">
+        <span className="project">Real Time Chat App</span>
+        <span className="project">Resume Generator</span>
+        <span className="project">Gith Repo App</span>
+        <span className="project">Expense Tracker</span>
+        <span className="project">Sugar Skull</span>
+      </div>
+      <div className="all-projects"></div>
     </div>
   );
 }
