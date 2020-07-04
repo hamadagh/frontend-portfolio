c349411c6ac53e24271b67339dd6ac2e4578e7ac
hamada <hamadagh777@gmail.com>
2020-07-05 00:27:10 +0200

    adding style to about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,24 +1,33 @@
 import React from "react";
-import Linkedin from "./images/linkedin.png";
-import Github from "./images/github.png";
-import Facebook from "./images/facebook.png";
-import Instagram from "./images/instagram.png";
+import Back from "./images/back.png";
+import { Link } from "react-router-dom";
 import "./style.css";
 
 function About() {
   return (
     <div className="about-page">
+      <div className="back-icon">
+        <Link to="/">
+          <img src={Back} alt="back" className="back-icon" />
+        </Link>
+      </div>
       <div className="about-me">
-        <span>
+        <span className="about-intro">
           I'm a Full-stack developer based in{" "}
           <span className="about-berlin">Berlin</span>
         </span>
-      </div>
-      <div className="about-icons">
-        <img src={Linkedin} alt="linkedin" />
-        <img src={Github} alt="Github" />
-        <img src={Facebook} alt="Facebook" />
-        <img src={Instagram} alt="Instagram" />
+        <br></br>
+        <div className="about-body-text">
+          <span className="about-text">
+            with a strong knowledge in building websites and <br></br>web
+            application using<span>Html</span>, <span>CSS</span>,{" "}
+            <span>JavaScript</span>, <span>React</span>, <br></br>
+            <span>Redux</span>, <span>Bootstrap</span>, <span>Node Js</span>,{" "}
+            <span>Express</span>, <span>MongoDB</span>, <br></br>
+            <span>SQL</span>, <span>WordPress</span>, <span>Photoshop</span>,{" "}
+            <span>Illustrator</span> ...{" "}
+          </span>
+        </div>
       </div>
     </div>
   );
