45823383f6da927efb2ea127b6790a6fffff923b
hamada <hamadagh777@gmail.com>
2020-07-04 23:52:15 +0200

    adding about me section in about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,25 @@
 import React from "react";
+import Linkedin from "./images/linkedin.png";
+import Github from "./images/github.png";
+import Facebook from "./images/facebook.png";
+import Instagram from "./images/instagram.png";
 import "./style.css";
 
 function About() {
   return (
     <div className="about-page">
-      <h1>hey</h1>
+      <div className="about-me">
+        <span>
+          I'm a Full-stack developer based in{" "}
+          <span className="about-berlin">Berlin</span>
+        </span>
+      </div>
+      <div className="about-icons">
+        <img src={Linkedin} alt="linkedin" />
+        <img src={Github} alt="Github" />
+        <img src={Facebook} alt="Facebook" />
+        <img src={Instagram} alt="Instagram" />
+      </div>
     </div>
   );
 }
