072a2082da9863371e01557b399389d88dee83c2
hamada <hamadagh777@gmail.com>
2020-07-13 16:01:06 +0200

    adding contact page component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,21 @@ import "./style.css";
 function Contact() {
   return (
     <div className="contact-page">
-      <p>hey</p>
+      <div className="contact-form">
+        <form>
+          <input type="text" value="name" />
+          <input type="text" value="email" />
+          <input type="text" value="message" />
+        </form>
+      </div>
+      <div className="contact-design">
+        <div className="contact-design-shape">
+          <h1>shape</h1>
+        </div>
+        <div className="contact-icons">
+          <h2>Icons</h2>
+        </div>
+      </div>
     </div>
   );
 }
