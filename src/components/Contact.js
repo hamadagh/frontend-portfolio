a3cc842a0569201509a57407ed5e3e7b5b8b4674
hamada <hamadagh777@gmail.com>
2020-07-13 16:35:49 +0200

    adding form in contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,15 +4,16 @@ import "./style.css";
 function Contact() {
   return (
     <div className="contact-page">
-      <div className="contact-form">
-        <form>
-          <input type="text" value="name" />
-          <input type="text" value="email" />
-          <input type="text" value="message" />
+      <div className="contact-left-part">
+        <form className="contact-form">
+          <input type="text" placeholder="name" />
+          <input type="text" placeholder="email" />
+          <input type="text" placeholder="message" />
+          <button>Send</button>
         </form>
       </div>
-      <div className="contact-design">
-        <div className="contact-design-shape">
+      <div className="contact-right-part">
+        <div className="contact-shape">
           <h1>shape</h1>
         </div>
         <div className="contact-icons">
