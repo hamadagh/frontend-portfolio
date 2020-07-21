2c14aa898d6ade4ee08c460ab2d2984d6e2e6d16
hamada <hamadagh777@gmail.com>
2020-07-21 23:00:57 +0200

    adding change color on click function

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useSpring, animated } from "react-spring";
-import Circle from "./images/circle.png";
-import Triangle from "./images/triangle.png";
-import Carre from "./images/carre.png";
 import Linkedin from "./images/linkedin.png";
 import Github from "./images/github.png";
 import Facebook from "./images/facebook.png";
@@ -16,28 +13,54 @@ const trans3 = (x, y) => `translate3d(${x / 6 - 490}px,${y / 6 - 213}px,0)`;
 const trans4 = (x, y) => `translate3d(${x / 6 + 20}px,${y / 6 - 65}px,0)`;
 
 function Home() {
-  const [checked, setChecked] = useState({ checked: "red" });
-  const [shape, setShape] = useState(`${Circle}`);
+  const [color, setColor] = useState({ color: "red" });
+  const [className, setClassName] = useState({
+    carre: "red-carre",
+    triangle: "red-triangle",
+    circle: "red-circle",
+  });
   const [props, set] = useSpring(() => ({
     xy: [0, 0],
     config: { mass: 10, tension: 550, friction: 140 },
   }));
 
-  const onValueChange = (e) => {
-    setChecked({ checked: e.target.value });
+  const changeColor = (e) => {
+    switch (color.color) {
+      case "red":
+        setColor({ color: "green" });
+        break;
+      case "green":
+        setColor({ color: "pink" });
+        break;
+      case "pink":
+        setColor({ color: "red" });
+        break;
+    }
   };
   useEffect(() => {
-    if (checked.checked === "green") {
-      setShape(`${Triangle}`);
+    if (color.color === "green") {
+      setClassName({
+        carre: "green-carre",
+        triangle: "green-triangle",
+        circle: "green-circle",
+      });
       document.documentElement.style.setProperty("--color", "#32ff00");
-    } else if (checked.checked === "pink") {
-      setShape(`${Carre}`);
+    } else if (color.color === "pink") {
+      setClassName({
+        carre: "pink-carre",
+        triangle: "pink-triangle",
+        circle: "pink-circle",
+      });
       document.documentElement.style.setProperty("--color", "#ff00f9");
-    } else if (checked.checked === "red") {
-      setShape(`${Circle}`);
+    } else if (color.color === "red") {
+      setClassName({
+        carre: "red-carre",
+        triangle: "red-triangle",
+        circle: "red-circle",
+      });
       document.documentElement.style.setProperty("--color", "#ff0000");
     }
-  }, [checked.checked]);
+  }, [color.color]);
 
   return (
     <div className="home-page">
@@ -49,25 +72,29 @@ function Home() {
           <animated.div
             className="hello-you"
             style={{ transform: props.xy.interpolate(trans1) }}
+            onClick={changeColor}
           >
-            <span className="hamada">Hello you !</span>
+            <span className="greeting">Hello you !</span>
           </animated.div>
           <div className="shapes">
             <animated.div
-              className="carre"
+              className={className.carre}
               style={{ transform: props.xy.interpolate(trans2) }}
             />
             <animated.div
-              className="triangle"
+              className={className.triangle}
               style={{ transform: props.xy.interpolate(trans4) }}
             />
             <animated.div
-              className="circle"
+              className={className.circle}
               style={{ transform: props.xy.interpolate(trans3) }}
             />
           </div>
         </div>
         <div className="full-stack-section">
+          <span className="im">
+            I'm <span className="hamada">Hamada,</span>
+          </span>
           <span className="full-stack">Full-stack developer</span>
           <br></br>
           <span className="from">
@@ -76,38 +103,7 @@ function Home() {
         </div>
       </div>
       <div className="third-section">
-        <form className="radio-buttons">
-          <div className="radio-button red-radio-button">
-            <input
-              type="radio"
-              value="red"
-              checked={checked.checked === "red"}
-              onChange={onValueChange}
-              id="red"
-            />
-            <span className="red-checkmark checkmark"></span>
-          </div>
-          <div className="radio-button green-radio-button">
-            <input
-              type="radio"
-              value="green"
-              checked={checked.checked === "green"}
-              onChange={onValueChange}
-              id="green"
-            />
-            <span className="green-checkmark checkmark"></span>
-          </div>
-          <div className="radio-button pink-radio-button">
-            <input
-              type="radio"
-              value="pink"
-              checked={checked.checked === "pink"}
-              onChange={onValueChange}
-              id="pink"
-            />
-            <span className="pink-checkmark checkmark"></span>
-          </div>
-        </form>
+        <div></div>
         <div className="icons">
           <a
             href="https://www.linkedin.com/in/ahmad-ghanem-822490182"
